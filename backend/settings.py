f3334321e2ddcfd93805e597fcca3ef07fe106f2
danie1zhang <55263240+danie1zhang@users.noreply.github.com>
2021-06-17 15:07:37 -0400

    Switch project to using MySQL in dev instead of SQLLITE (#44)

--- backend/settings.py
+++ backend/settings.py
@@ -76,8 +76,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME', 'canvas_app_explorer_local'),
+        'USER': os.getenv('DB_USER', 'cae_user'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'cae_pw'),
+        'HOST': os.getenv('DB_HOST', 'canvas_app_explorer_mysql'),
+        'PORT': os.getenv('DB_PORT', '3306'),
+        'OPTIONS': {'charset': 'utf8mb4'},
+        'TEST': {
+            'CHARSET': 'utf8mb4',
+            'COLLATION': 'utf8mb4_unicode_ci'
+        }
     }
 }
 
