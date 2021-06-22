be718481c1bc37ace909e854a1da3a1f4bdcc794
Matthew Jones <jonespm@gmail.com>
2021-06-22 15:44:07 -0400

    Fixes #55 Adding a __str__ method to each model class so it will display the name

--- backend/canvas_app_explorer/models.py
+++ backend/canvas_app_explorer/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class CanvasPlacement(models.Model):
     name = models.CharField(max_length=150)
+    def __str__(self):
+        return self.name
 
 class LtiTool(models.Model):
     name = models.CharField(max_length=50)
@@ -14,3 +16,6 @@ class LtiTool(models.Model):
     privacy_agreement = models.TextField()
     support_resources = models.TextField()
     canvas_placement = models.ManyToManyField(CanvasPlacement)
+
+    def __str__(self):
+        return self.name
