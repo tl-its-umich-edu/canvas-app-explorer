868be3b6be362708d99b20485a084fd5046cdeb3
danie1zhang <55263240+danie1zhang@users.noreply.github.com>
2021-07-09 10:30:54 -0400

    Fixes #60 Consumes REST API using React to generate Canvas App Components (#61)

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
+  presets: [
+    ['@babel/preset-env', {
+        "targets": {"browsers": [ "since 2017-06"]}
+    }], 
+    '@babel/preset-react'],
 };
