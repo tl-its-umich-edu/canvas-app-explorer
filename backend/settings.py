2955a1439ba6e0ff778bef445579cc066ed11760
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2021-08-03 12:27:10 -0400

    Switching BigAutoField down to AutoField as the default

--- backend/settings.py
+++ backend/settings.py
@@ -132,6 +132,6 @@ WEBPACK_LOADER = {
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 DEFAULT_FILE_STORAGE = 'canvas_app_explorer.storage_get_file.DatabaseFileStorage'
