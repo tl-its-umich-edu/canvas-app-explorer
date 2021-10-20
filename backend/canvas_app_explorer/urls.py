e6648e00700acb691b5539f84a159159ad774975
dlzhang <dlzhang@umich.edu>
2021-10-20 15:19:55 -0400

    added to urls.py

--- backend/canvas_app_explorer/urls.py
+++ backend/canvas_app_explorer/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('lti/launch/', lti1p3.launch, name='lti_launch'),
     path('lti/jwks/', lti1p3.get_jwks, name='lti_get_jwks'),
     path('lti/config/', lti1p3.generate_config_json, name=lti1p3.generate_config_json.__name__),
-    ]
+    path('tinymce/', include('tinymce.urls')),
+]
