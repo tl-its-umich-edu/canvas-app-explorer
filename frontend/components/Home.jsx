e37b1819c2d954ac99707213494e820291931b8e
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2021-12-02 16:23:29 -0500

    Fixes #105 Tiny MCE causes text on cards to become HTML code (#108)

--- frontend/components/Home.jsx
+++ frontend/components/Home.jsx
@@ -205,7 +205,7 @@ function Home__RenderFunc(props,ref) {
                                                     "Home__text__c1Y5M"
                                                   )}
                                                 >
-                                                  {tool.short_description}
+                                                  <span dangerouslySetInnerHTML={{ __html: tool.short_description}} />
                                                 </div>
                                             }
                                             image={
@@ -293,10 +293,8 @@ function Home__RenderFunc(props,ref) {
                                                 >
                                                     <span>
                                                         <span style={{ fontWeight: 700 }}>{"Description"}</span>
-                                                        <React.Fragment>
-                                                            {"\n"}    
-                                                            {tool.long_description}
-                                                        </React.Fragment>
+                                                        <span dangerouslySetInnerHTML={{ __html: tool.long_description}} />
+
                                                     </span>
                                                 </div>
                                             }
@@ -313,11 +311,7 @@ function Home__RenderFunc(props,ref) {
                                                     <span style={{ fontWeight: 700 }}>
                                                       {"Privacy Agreement"}
                                                     </span>
-                                                    <React.Fragment>
-                                                      {"\n"}
-                                                      {tool.privacy_agreement}
-                                                      {"\n"}
-                                                    </React.Fragment>
+                                                    <span dangerouslySetInnerHTML={{ __html: tool.privacy}} />
                                                   </span>
                                                 </div>
                                             }
@@ -353,10 +347,7 @@ function Home__RenderFunc(props,ref) {
                                                         <span style={{ fontWeight: 700 }}>
                                                             {"Support Resources"}
                                                         </span>
-                                                        <React.Fragment>
-                                                            {"\n"}
-                                                            {tool.support_resources}
-                                                        </React.Fragment>
+                                                        <span dangerouslySetInnerHTML={{ __html: tool.support_resources}} />
                                                     </span>
                                                 </div>
                                             }
