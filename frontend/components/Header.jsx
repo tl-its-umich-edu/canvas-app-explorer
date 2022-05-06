40e6934ecd0a77de8720f64e946179184c1e677b
Sam Sciolla <ssciolla@umich.edu>
2022-05-06 11:10:54 -0400

    Tidy up, refactor filter input

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FilledInput, Grid, InputAdornment, makeStyles, Paper, Typography } from '@material-ui/core';
+import { FilledInput, Grid, InputAdornment, makeStyles, Paper, TextField, Typography } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles((theme) => ({
@@ -16,30 +16,44 @@ const useStyles = makeStyles((theme) => ({
     background: theme.palette.primary.contrastText
   },
   inputText: {
-    fontSize: '25px'
+    fontSize: '20px'
   }
 }));
 
 export default function Header (props) {
-  const classes = useStyles()
+  const classes = useStyles();
   return (
     <Grid container className={classes.titleBar} spacing={2} justifyContent='center'>
       <Grid item md={5} sm={12}>
         <Typography variant='h4' component='h1'>Canvas App Explorer</Typography>
       </Grid>
-      <Grid item md={5} sm={12} className={classes.filterField} component={Paper}>
-        <FilledInput
-          variant='filled'
-          classes={{ input: classes.inputText }}
-          className={classes.filterField}
-          fullWidth
-          color='primary'
-          id='tool-filter'
-          placeholder='Filter by name'
-          aria-label='Filter tools by name'
-          onChange={(e) => props.onFilterChange(e.target.value)}
-          startAdornment={<SearchIcon fontSize='medium' className={classes.searchIcon} />}
-        />
+      <Grid
+        item
+        container
+        md={5}
+        sm={12}
+        className={classes.filterField}
+        component={Paper}
+        alignItems='flex-end'
+      >
+        <Grid container justifyContent='flex-start' alignItems='center'>
+          <Grid item xs='auto'>
+            <SearchIcon fontSize='medium' className={classes.searchIcon} />
+          </Grid>
+          <Grid item xs>
+            <TextField
+              id='tool-filter'
+              placeholder='Filter by name or description'
+              aria-label='Filter tools by name or description'
+              InputProps={{ classes: { input: classes.inputText } }}
+              variant='standard'
+              className={classes.filterField}
+              color='primary'
+              fullWidth
+              onChange={(e) => props.onFilterChange(e.target.value)}
+            />
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
