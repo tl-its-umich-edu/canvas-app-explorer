28f4e2af998940bf78d42414786059851666affe
Sam Sciolla <ssciolla@umich.edu>
2022-05-24 15:53:28 -0400

    Update webpack; use webpack-merge approach; switch to TypeScript; remove Babel; add ESLint

---
+++ frontend/webpack/webpack.common.ts
@@ -0,0 +1,44 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+import BundleTrackerPlugin from 'webpack-bundle-tracker';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+const commonConfig: webpack.Configuration = {
+  context: path.resolve(__dirname, '..'),
+  entry: path.resolve('components', 'index'),
+  output: {
+    path: path.resolve('./bundles/'),
+    filename: "[name]-[fullhash].js",
+    chunkFilename: "[name]-[chunkhash].js"
+  },
+
+  plugins: [
+    new BundleTrackerPlugin({ filename: './webpack-stats.json' }),
+    new MiniCssExtractPlugin({
+      filename: '[name]-[fullhash].css',
+      chunkFilename: '[name]-[chunkhash].css',
+    }),
+  ],
+
+  module: {
+    rules: [
+      // we pass the output from babel loader to react-hot loader
+      {
+        test: /\.(js|jsx|tsx|ts)?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ],
+  },
+
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  }
+}
+
+export default commonConfig
