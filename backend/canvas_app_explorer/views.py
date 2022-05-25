d3742d34da21e40304a7fccd4a321def222fd731
Sam Sciolla <35741256+ssciolla@users.noreply.github.com>
2022-05-25 16:26:42 -0400

    Rework `LtiToolViewSet` to prevent multiple API calls (#172) (#173)
    * Rework LtiToolViewSet to prevent multiple API calls
    
    * Add return type to list method

--- backend/canvas_app_explorer/views.py
+++ backend/canvas_app_explorer/views.py
@@ -1,39 +1,35 @@
+import logging
+
 from canvas_oauth.oauth import get_oauth_token
 from django.conf import settings
 from rest_framework import permissions, viewsets
 from rest_framework.request import Request
+from rest_framework.response import Response
 
 from backend.canvas_app_explorer import models, serializers
 from .canvas_lti_manager import CanvasLtiManager
 
-class LTIToolViewSet(viewsets.ModelViewSet):
+logger = logging.getLogger(__name__)
+
+class LTIToolViewSet(viewsets.ViewSet):
     """
     API endpoint that lists LTI tools available in the course context
     """
-    serializer_class = serializers.LtiToolSerializer
-    permission_classes = [permissions.DjangoModelPermissions]
-    queryset = models.LtiTool.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
 
-    def get_serializer_context(self):
-        """
-        Extends context for LtiToolSerializer to include data (from the Canvas API) about tools available
-        in the course and whether they are enabled for the navigation
-        """
-        context = super(LTIToolViewSet, self).get_serializer_context()
-        request: Request = context['request']
+    def list(self, request: Request) -> Response:
         access_token = get_oauth_token(request)
+        logger.debug(f"Course ID: {request.session['course_id']}")
         manager = CanvasLtiManager(
             f'https://{settings.CANVAS_OAUTH_CANVAS_DOMAIN}',
             access_token,
             request.session['course_id']
         )
-        context['available_tools'] = manager.get_tools_available_in_course()
-        return context
-
-    def get_queryset(self):
-        """
-        Overrides default queryset to limit results to LtiTool model instances with a non-null Canvas ID
-        and with matching data from Canvas indicating it is available in the course.
-        """
-        available_tool_ids = [t.id for t in self.get_serializer_context()['available_tools']]
-        return models.LtiTool.objects.filter(canvas_id__isnull=False, canvas_id__in=available_tool_ids)
+        available_tools = manager.get_tools_available_in_course()
+        logger.debug('available_tools: ' + ', '.join([tool.__str__() for tool in available_tools]))
+        available_tool_ids = [t.id for t in available_tools]
+        queryset = models.LtiTool.objects.filter(canvas_id__isnull=False, canvas_id__in=available_tool_ids)
+        serializer = serializers.LtiToolSerializer(
+            queryset, many=True, context={ 'available_tools': available_tools }
+        )
+        return Response(serializer.data)
