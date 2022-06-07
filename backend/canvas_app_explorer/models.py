902dc616d641ca0aa89532dd8bdb11901b6166fc
Matthew Jones <jonespm@gmail.com>
2022-06-07 15:57:16 -0400

    Fixes #179 - Don't consider HTML formatting in character count limits

--- backend/canvas_app_explorer/models.py
+++ backend/canvas_app_explorer/models.py
@@ -1,9 +1,30 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.deconstruct import deconstructible
+from django.utils.html import strip_tags
 from db_file_storage.model_utils import delete_file, delete_file_if_needed
 from tinymce.models import HTMLField
 
-# Create your models here.
+# Based on the built in validators 
+# https://github.com/django/django/blob/main/django/core/validators.py
+# Use in your model as validators=[MaxLenIgnoreHTMLValidator(max_length=120)]
+@deconstructible
+class MaxLenIgnoreHTMLValidator:
+    max_length = 120
+    def __init__(self, max_length=None):
+        pass
+        if max_length is not None:
+            self.max_length = max_length
 
+    def __call__(self, value: str):
+        # Return the value not considering HTML tags that won't be displayed
+        value = strip_tags(value.strip())
+        if len(value) > self.max_length:
+            raise ValidationError(
+                (f'This field without tags is {len(value)}, greater than max length of {self.max_length}'),
+            )
+
+### Models are below
 class CanvasPlacement(models.Model):
     name = models.CharField(max_length=150)
     def __str__(self):
@@ -26,7 +47,7 @@ class LtiTool(models.Model):
     logo_image_alt_text = models.CharField(max_length=255, blank=True, null=True)
     main_image = models.ImageField(upload_to='canvas_app_explorer.MainImage/bytes/filename/mimetype', blank=True, null=True)
     main_image_alt_text = models.CharField(max_length=255, blank=True, null=True)
-    short_description = HTMLField(max_length=120)
+    short_description = HTMLField(validators=[MaxLenIgnoreHTMLValidator(max_length=120)])
     long_description = HTMLField()
     privacy_agreement = HTMLField()
     support_resources = HTMLField()
