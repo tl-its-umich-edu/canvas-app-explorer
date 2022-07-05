9c7c2b9af2d7b5b259bffe79ea88d7e433f4c8ea
zqian <zqian@umich.edu>
2022-07-05 10:42:33 -0400

    removed unused import and unused variables

--- backend/views.py
+++ backend/views.py
@@ -4,7 +4,6 @@ from canvas_oauth.oauth import get_oauth_token, handle_missing_token
 from canvas_oauth.models import CanvasOAuth2Token
 from canvas_oauth.exceptions import InvalidOAuthReturnError
 
-from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
@@ -14,8 +13,8 @@ logger = logging.getLogger(__name__)
 @login_required
 def get_home_template(request):
     try:
-        access_token = get_oauth_token(request)
-    except InvalidOAuthReturnError as err:
+        get_oauth_token(request)
+    except InvalidOAuthReturnError:
         logger.error(f"InvalidOAuthReturnError for user: {request.user}. Remove invalid refresh_token and prompt for reauthentication.")
         CanvasOAuth2Token.objects.filter(user=request.user).delete()
         return handle_missing_token(request)
