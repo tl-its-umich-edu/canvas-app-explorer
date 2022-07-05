b8410fa8ebfead949cc38eeaff9d6a0844482b87
Zhen Qian <zqian@umich.edu>
2022-07-05 10:49:49 -0400

    issue_208 remove invalid refresh token and redirect user to the OAuth page for new token (#211)
    * issue_208 remove invalide refresh token and redirect user to the OAuth page
    
    * Update backend/views.py, move import logging ahead of external library imports
    
    Co-authored-by: Sam Sciolla <35741256+ssciolla@users.noreply.github.com>
    
    * iissue-208 added detailed Canvas OAuth exception types for error handlings
    
    * #208 leave only InvalidOAuthReturnError section
    
    let the django-canvas-oauth library's middleware.py handle other exceptions
    
    * issue #208 added user info into the log message
    
    * removed unused import and unused variables
    
    Co-authored-by: Sam Sciolla <35741256+ssciolla@users.noreply.github.com>

--- backend/views.py
+++ backend/views.py
@@ -1,8 +1,22 @@
-from canvas_oauth.oauth import get_oauth_token
+import logging
+
+from canvas_oauth.oauth import get_oauth_token, handle_missing_token
+from canvas_oauth.models import CanvasOAuth2Token
+from canvas_oauth.exceptions import InvalidOAuthReturnError
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+
+logger = logging.getLogger(__name__)
+
 @login_required
 def get_home_template(request):
-    access_token = get_oauth_token(request)
+    try:
+        get_oauth_token(request)
+    except InvalidOAuthReturnError:
+        logger.error(f"InvalidOAuthReturnError for user: {request.user}. Remove invalid refresh_token and prompt for reauthentication.")
+        CanvasOAuth2Token.objects.filter(user=request.user).delete()
+        return handle_missing_token(request)
+    
     return render(request, 'home.html')
