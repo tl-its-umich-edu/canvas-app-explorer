4bb36cce713d3b4842656e22fde9871770f1a1df
Sam Sciolla <35741256+ssciolla@users.noreply.github.com>
2022-07-07 19:03:00 -0400

    Add icon with tooltip indicator of tool's inclusion in the navigation (#230) (#237)
    * Add icon/tooltip indicator of navigation inclusion; create constants.ts
    
    * Create a constants object
    
    * Update frontend/app/constants.ts

--- frontend/app/components/Home.tsx
+++ frontend/app/components/Home.tsx
@@ -7,6 +7,7 @@ import ErrorsDisplay from './ErrorsDisplay';
 import HeaderAppBar from './HeaderAppBar';
 import ToolCard from './ToolCard';
 import { getTools } from '../api';
+import constants from '../constants';
 import '../css/Home.css';
 import { Globals, Tool } from '../interfaces';
 
@@ -101,7 +102,7 @@ function Home (props: HomeProps) {
       </Typography>
       <Snackbar open={showRefreshAlert} onClose={handleRefreshAlertClose} autoHideDuration={10000}>
         <Alert severity='info' elevation={2} onClose={handleRefreshAlertClose}>
-          Refresh the page to update the tool&apos;s appearance in the left-hand navigation.
+          Refresh the page to update the tool&apos;s appearance in the {constants.toolMenuName}.
         </Alert>
       </Snackbar>
     </div>
---
+++ frontend/app/constants.ts
@@ -0,0 +1,5 @@
+const constants = Object.freeze({
+  toolMenuName: 'Course Navigation Menu'
+});
+
+export default constants;
