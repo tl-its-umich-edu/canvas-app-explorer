6cc2a7907425786b94884e0c55d46a8267b36693
Sam Sciolla <35741256+ssciolla@users.noreply.github.com>
2022-07-18 09:59:47 -0400

    Provide default Canvas API scopes for OAuth, with an environment variable escape hatch (#202) (#245)
    * Specify the Canvas API scopes needed by the application, with an environment variable escape hatch
    
    * Remove trailing comma
    
    * Add current default scopes to commented-out env variable

---
+++ backend/canvas_scopes.py
@@ -0,0 +1,7 @@
+DEFAUlT_CANVAS_SCOPES = [
+    # Courses
+    'url:GET|/api/v1/courses/:id',
+    # Tabs
+    'url:GET|/api/v1/courses/:course_id/tabs',
+    'url:PUT|/api/v1/courses/:course_id/tabs/:tab_id'
+]
